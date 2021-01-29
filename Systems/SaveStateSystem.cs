ecd31f3c70563e0266f34e832b09394e5fefa424
Aladji16 <aladji.faye@polymtl.ca>
2021-01-29 14:59:00 -0500

    Ajout liste d'états

--- Systems/SaveStateSystem.cs
+++ Systems/SaveStateSystem.cs
@@ -3,35 +3,61 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+//état pour chaque cercle
+public class CircleState
+{
+    private ColorComponent colorComponent;
+    private PositionComponent positionComponent;
+    private SizeComponent sizeComponent;
+    private SpeedComponent speedComponent;
+    private TypeComponent typeComponent;
+    
+    public CircleState(ColorComponent color, PositionComponent pos, SizeComponent size, SpeedComponent speed, TypeComponent type)
+    {
+        colorComponent = color;
+        positionComponent = pos;
+        sizeComponent = size;
+        speedComponent = speed;
+        typeComponent = type;
 
+    }
+}
 
 public class SaveStateSystem : ISystem
 {
-    //private uint compteur = 0;
-    //private bool isInit = false;
-    //private ECSManager manager = ECSManager.Instance;
+
+    //private int frameCounter = 1;
+    private List<List<CircleState>> savedStates = new List<List<CircleState>>();
 
     public void UpdateSystem()
     {
-        //    if (!isInit)
-        //    {
-        //        foreach (Config.ShapeConfig config in manager.Config.allShapesToSpawn)
-        //        {
-        //            manager.CreateShape(compteur, config);
-        //            manager.UpdateShapePosition(compteur, config.initialPos);
-        //            //à ajouter: la définition des components vitesse, position et taille
-        //            compteur++;
-        //        }
-        //        isInit = true;
-        //        Debug.Log("Compteur " + compteur);
+
+        if (savedStates.Count == 60) //si on a atteint une taille de 60, on a donc atteint 2 secondes de simulation
+        {
+            savedStates.RemoveAt(0); //on enlève l'élément d'il y a deux secondes, soit le premier élément de la liste
+
+        }
+
+        List<CircleState> actualState = new List<CircleState>();
+        for (int i = 0; i < world.PositionComponents.Count; i++)
+        {
+            ColorComponent color = world.ColorComponent[i];
+            PositionComponent pos = world.PositionComponent[i];
+            SizeComponent size = world.SizeComponent[i];
+            SpeedComponent speed = world.SpeedComponent[i];
+            TypeComponent type = world.TypeComponent[i];
+            actualState.Add(new CircleState(color, pos, size, speed, type));
+        }
+
+        savedStates.Add(actualState);
+
+        //frameCounter += 1;
+
+
+
+
     }
 
-    //}
-
-    //public void createCircle()
-    //{
-
-    //}
 
     public String Name => "SaveStateSystem";
 }
