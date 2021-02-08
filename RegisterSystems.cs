982484d370b4501f4118c701a680dee20180c8af
jacksonlli <59549394+jacksonlli@users.noreply.github.com>
2021-02-08 00:07:13 -0500

    ordre dappel

--- RegisterSystems.cs
+++ RegisterSystems.cs
@@ -12,14 +12,13 @@ public class RegisterSystems
         toRegister.Add(new InitializationSystem());
         toRegister.Add(new PositionUpdateSystem(false));
         toRegister.Add(new CollisionSystem(false));
-        toRegister.Add(new SaveStateSystem());
-        toRegister.Add(new RestoreStateSystem());
         for (int i = 0; i < 4; i++)
         {
             toRegister.Add(new PositionUpdateSystem(true));
             toRegister.Add(new CollisionSystem(true));
         }
-            
+        toRegister.Add(new SaveStateSystem());
+        toRegister.Add(new RestoreStateSystem());
         return toRegister;
     }
 }
